a1090c8f5ea726ea325d2c2d47d19f29bc12a49b
Pepi <yukiponu@gmail.com>
2025-10-04 10:12:08 +1000

    Add project files.

---
+++ WalkRallyApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WalkRallyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
